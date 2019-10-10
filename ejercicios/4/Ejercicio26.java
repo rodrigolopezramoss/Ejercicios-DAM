65f229b1f508d847da0aa47905cea7ca0dfd6b71
Rodrigo López Ramos <rodrilr85@gmail.com>
2019-10-10 09:13:10 +0200

    Actualizado+++

--- ejercicios/4/Ejercicio26.java
+++ ejercicios/4/Ejercicio26.java
@@ -7,41 +7,78 @@ public class Ejercicio26{
 
     System.out.println("Introduze el número de personas:");
     linea = System.console().readLine();
-    int a;
-    a=Integer.parseInt( linea );
+    int p;
+    p=Integer.parseInt( linea );
 
     String d;
     System.out.println("Introduze ahora el día:");
     d = System.console().readLine();
  
     String t;
-    System.out.println("¿Tienes tarje ta CineCampa?(s/n)");
+    System.out.println("¿Tienes tarjeta CineCampa?(s/n)");
     t = System.console().readLine(); 
     
-    int e =8; 
-     
+    int e =8;
+    int a =e*p;
+    
+    System.out.println("Gracias, aquí tiene.");
+    System.out.println("Entradas individuales: "+p);
+    
     switch (d){
       case "miércoles":
 		e= 5;
+		    System.out.println("Precio por entrada individual: "+e);
         break;
       case "jueves":
-		e= 0;
+		if (p%2==0){
+		  e=11*(p/2);
+		  System.out.println("Precio por entrada por parejas: "+11);
+          System.out.println("Total: "+(e));
+
+        } else {
+	  	  e=(((p-1)/2)*11);
+	  	  System.out.println("Precio por entrada por parejas: "+11);
+          System.out.println("Precio por entrada individual: "+8);
+          System.out.println("Total: "+(e+8));
+          
+
+        }
         break;
       default:
-        System.out.println("Pues nada...");
-    }
+      System.out.println("Entradas individuales: "+p);
+      System.out.println("Precio por entrada individual: "+e);
+      System.out.println("Total: "+ (e*p));
 
+      
+    }
+    double de=0;
     switch (t){
       case "s":
-		e= 5;
+		de= e*0.10;
         break;
       case "n":
-		e= 0;
+		de= 0;
         break;
       default:
         System.out.println("Pues nada...");
     }
+    
+    System.out.println("Descuento de: "+de);
+        switch (d){
+      case "miércoles":
+	    System.out.println("A pagar: "+(e-de));
+        break;
+      case "jueves":
+      		if (p%2==0){
+			    System.out.println("A pagar: "+(e-de));	
+        } else {
+	            System.out.println("A pagar: "+((e+8)-de));
+        }
+        break;
+      default:
+    System.out.println("A pagar: "+((e*p)-de));
 
-    System.out.println("Gracias. Aquí tiene el desglose de su compra");        
+      
+    }
   }
 }
---
+++ ejercicios/4/Ejercicio27.java
@@ -0,0 +1,58 @@
+public class Ejercicio27{
+  public static void main(String[] args){
+
+    String s;
+
+    System.out.println("¿Quieres una tarta?");
+
+    System.out.println("Introduze el sabor(manzana, fresa o chocolate)");
+    s = System.console().readLine();
+
+    String n;
+    System.out.println("¿Con nata?(s/n)");
+    n = System.console().readLine();
+ 
+    String o;
+    System.out.println("¿Con un nombre?(s/n)");
+    o = System.console().readLine(); 
+    
+    double p = 0;
+    
+    switch (s){
+      case "manzana":
+      p = 18;
+
+        break;
+      case "chocolate":
+      String c;
+      System.out.println("¿Chocolate negro o blanco?");
+      c = System.console().readLine();
+      
+      if (c.equals("negro")){
+		  p = 14;
+		  } else{
+			  p = 15;
+			  }  
+	    break;	    
+      case "fresa":
+      p = 16;
+        break;
+      default:
+     }
+     System.out.println("Tarta sabor "+s+": "+p+"€");
+     
+     double nat= 0;
+     double nom= 0;
+     if(n .equals("s")){
+	   nat=2.50;
+       System.out.println("Con nata: "+nat+"€");
+     }
+     if(o .equals("s")){
+	   nom=2.75;	 
+       System.out.println("Con nombre: "+nom+"€");
+     }
+     
+     System.out.println("Total: "+(p+nat+nom)+"€");     
+  }
+}
+
