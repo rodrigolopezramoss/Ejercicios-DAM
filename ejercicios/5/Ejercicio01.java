f14fba43d6c8bc23374c13210cd4d1e7dcd50ad6
Rodrigo López Ramos <rodrilr85@gmail.com>
2019-10-10 10:20:22 +0200

    Subida de ejercicios 1 y 2: numeración de múltiplos de 5.

---
+++ ejercicios/5/Ejercicio01.java
@@ -0,0 +1,10 @@
+public class Ejercicio01{
+  public static void main(String [] args){
+    
+    for(int c = 0; c<=100; c+=5){
+    
+    System.out.println(c);
+    
+    }
+  }
+}
---
+++ ejercicios/5/Ejercicio02.java
@@ -0,0 +1,11 @@
+public class Ejercicio02{
+  public static void main(String [] args){
+    
+    int c = 0;
+    
+    while(c<=100){
+    System.out.println(c);
+    c+=5;
+    }
+  }
+}
