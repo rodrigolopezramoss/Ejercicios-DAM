198c5c6f42561cbda208b0684c8a78476e0a35b3
Rodrigo López Ramos <rodrilr85@gmail.com>
2019-11-07 10:04:21 +0100

    Subida de los ejercicios 1 y 2: arrays iniciales

---
+++ ejercicios/7/Ejercicio01.java
@@ -0,0 +1,18 @@
+public class Ejercicio01{
+  public static void main (String[]args){
+  
+  int[] x = new int[12];
+  
+  x[0]= 39;
+  x[1]= -2;
+  x[4]= 0;
+  x[7]= 14;
+  x[8]= 5;
+  x[9]= 120;
+  
+  for(int i = 0; i <= 11; i++){
+    System.out.printf("x[%d]: %d\n",i,x[i]);
+  }
+  
+  }
+}
